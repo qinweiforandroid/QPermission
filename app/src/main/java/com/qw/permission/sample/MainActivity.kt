21f54e721dd7ea9f8ee2797960651ff632c57d2b
qinwei <qinwei_it@163.com>
2022-04-16 18:19:35 +0800

    update gradle version gradle-7.2

--- app/src/main/java/com/qw/permission/sample/MainActivity.kt
+++ app/src/main/java/com/qw/permission/sample/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.qw.permission.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import com.qw.permission.*
 import com.qw.permission.sample.databinding.ActivityMainBinding
 
